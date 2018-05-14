8439357d31f11ca0cdac88ff8c0fd9782ffe492d
Nihanth Subramanya <nhnt11@gmail.com>
2018-05-14 19:00:50 +0200

    Update popup notification content, still not pretty though

--- api/api.js
+++ api/api.js
@@ -64,60 +64,33 @@ function warnIfNeeded(browser, host) {
 
   let doc = browser.ownerDocument;
 
-  function showPopupNotification(aMessage, aTextboxCallback, aTextboxPlaceholder) {
-    let panel = doc.defaultView.PopupNotifications.panel;
-    panel.addEventListener("popupshown", function() {
-      let n = doc.getElementById("breach-alerts-notification");
-      let box = doc.getAnonymousElementByAttribute(n, "class", "popup-notification-body");
-      let i = doc.createElementNS("http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul", "textbox");
-      i.setAttribute("placeholder", aTextboxPlaceholder);
-      i.addEventListener("keydown", function listener(aEvent) {
-        if (aEvent.key != "Enter") {
-          return;
-        }
-
-        aTextboxCallback(i.value);
-        i.removeEventListener("keydown", listener);
-        i.remove();
-      });
-      box.appendChild(i);
-    }, { once: true });
-
-    doc.defaultView.PopupNotifications.show(
-      browser, "breach-alerts", aMessage,
-      null, null, null, {persistent: true});
-  }
-
-  if (host == "haveibeenpwned.com") {
-    showPopupNotification("Use the textbox below to simulate a breach!", aText => {
-      let xhr = new XMLHttpRequest();
-      xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          doc.defaultView.alert("Breach alerts prototype: breach triggered successfully!");
-        }
-      };
-      let addUserURL = Services.prefs.getCharPref("extensions.breachalerts.serverURL", kDefaultServerURL) + "/user/breached";
-      xhr.open("POST", addUserURL, true);
-      xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-      xhr.send(JSON.stringify({ emails: aText.split(",").map(e => e.trim()) }));
-    }, "Enter email addresses separated by commas and press enter");
-    return;
-  }
-
   warnedHostSet.add(host);
 
-  showPopupNotification("You visited hacked site " + host + "!", aText => {
-    let xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        doc.defaultView.alert("Breach alerts prototype: user successfully registered with server.");
-      }
-    };
-    let addUserURL = Services.prefs.getCharPref("extensions.breachalerts.serverURL", kDefaultServerURL) + "/user/add";
-    xhr.open("POST", addUserURL, true);
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhr.send(JSON.stringify({ email: aText }));
-  }, "Enter email address and press enter");
+  let panel = doc.defaultView.PopupNotifications.panel;
+  panel.addEventListener("popupshown", function() {
+    let n = doc.getElementById("breach-alerts-notification");
+    let box = doc.getAnonymousElementByAttribute(n, "class", "popup-notification-body");
+    let elt = doc.createElementNS("http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul", "description");
+    elt.setAttribute("value", "This website has been breached!");
+    elt.setAttribute("style", "font-size: 1.3rem;");
+    box.appendChild(elt);
+    elt = doc.createElementNS("http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul", "description");
+    elt.setAttribute("value", "Quick information about what a breach is, what it means and how it affects people.");
+    box.appendChild(elt);
+    elt = doc.createElementNS("http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul", "description");
+    elt.setAttribute("value", "Why you should go to the website and scan all of your usernames. Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+    box.appendChild(elt);
+  }, { once: true });
+
+  doc.defaultView.PopupNotifications.show(
+    browser, "breach-alerts", "",
+    null, {
+      label: "Find Out More",
+      accessKey: "f",
+      callback: () => {
+        doc.defaultView.openTrustedLinkIn("http://fx-breach-alerts.herokuapp.com/?breach=" + host, "tab");
+      },
+    }, [{label: "Dismiss", accessKey: "d", callback: () => {}}], {persistent: true});
 }
 
 function startup(aData, aReason) {
